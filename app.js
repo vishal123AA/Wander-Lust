076cfd01b79d3abb7b877e679a320477f9457da0
vishal <vishalkumarkalher16@gmail.com>
2026-06-17 12:46:12 +0530

    this is the main file here the flow starts all the routes and packages are require here

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+const path = require('path');
+const Listing = require('./models/listing');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+const mongoose = require('mongoose');
+const Mongo_URL = 'mongodb://localhost:27017/Wander-Lust';
+
+main()
+    .then(() => {
+        console.log("MongoDB connected successfully");
+    })
+    .catch((err) => {
+        console.log("MongoDB connection failed");
+        console.log(err);
+    });
+
+async function main() {
+    await mongoose.connect(Mongo_URL);
+}
+
+app.get('/', (req, res) => {
+    res.send("this is the base route");
+});
+
+app.get('/testListings', async (req, res) => {
+    let SampleListings = new Listing({
+        title: "Cozy Cottage",
+        description: "A charming cottage nestled in the countryside.",
+        price: 150,
+        location: "Countryside",
+        country: "USA"
+    });
+    await SampleListings.save();
+    res.send("Test listing created!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
