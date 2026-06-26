1b2ffa5977d4a83affef634b1aaea1b35f62ef4c
vishal <vishalkumarkalher16@gmail.com>
2026-06-26 16:51:04 +0530

    use the middleware for express to understand json data

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ app.use(methodOverride('_method'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
