da53836abbcc28bf1f2c042febc3557c0c19fd05
vishal <vishalkumarkalher16@gmail.com>
2026-07-04 18:30:32 +0530

    this is the review model in which we have diiferent reviews for all listings

---
+++ models/review.js
@@ -0,0 +1,22 @@
+const mongoose = requiure("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    comment :{
+        type: String,
+        required: true
+    },
+    rating :{
+        type :Number,
+        min:1,
+        max:5,
+        required : true
+    },
+    createdAt : {
+        type :Date,
+        default:Date.now()
+    }
+});
+
+const Review = mongoose.model("Review",reviewSchema);
+module.exports = Review;
