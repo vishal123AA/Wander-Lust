5618a09998f347a8a7c92fcab5a0bbd417f1e7ef
vishal <vishalkumarkalher16@gmail.com>
2026-07-06 21:38:14 +0530

    changes in how we require the listingSchema for validations

--- routes/listing.js
+++ routes/listing.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Listing = require('../models/listing');
-const listingSchema = require("../schema.js"); 
+const {listingSchema} = require("../schema.js"); 
 
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
