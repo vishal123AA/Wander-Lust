861d54be3f0949d2d510ea699332a0456a57629e
vishal <vishalkumarkalher16@gmail.com>
2026-07-06 18:26:16 +0530

    in this file we use express router for all the listings routes

---
+++ routes/listing.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const router = express.Router();
+
+const Listing = require('./models/listing');
+const listingSchema = require("./schema.js"); 
+
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+
+const validateListing = (req,res,next)=>{
+    let {error} =  listingSchema.validate(req.body);
+      if(error){
+        let errMsg = error.details.map((el) =>  el.message).join(",");
+        throw new ExpressError(400,errMsg);
+      }
+      else{
+        next();
+      }
+}
+
+//index route
+router.get("/", wrapAsync(async(req,res,next) => {
+    let allListings =  await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
+}));
+
+router.get("/new",(req,res) => {
+    res.render("listings/new.ejs");
+});
+
+// show route
+router.get("/:id",wrapAsync(async(req,res) => {
+    let {id} = req.params;
+    let listing = await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs", {listing});
+}));
+
+//add listing
+router.post("/",validateListing,wrapAsync(async(req,res,next) => {
+    // let {title,description,image,price,location,country} = req.body;
+    // let newListing = new Listing({
+    //     title:title,
+    //     description:description,
+    //     image:image,
+    //     price:price,
+    //     location:location,
+    //     country:country
+    // });
+    // await newListing.save();
+      const newListings = new Listing(req.body.listing);
+      await newListings.save();
+      res.redirect("/listings");
+
+}));
+
+//edit
+router.get("/:id/edit",wrapAsync(async(req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+}));
+
+//update
+router.put("/:id",validateListing ,wrapAsync(async(req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+}));
+
+//delete
+router.delete("/:id", wrapAsync(async(req, res) =>{
+    let {id} = req.params;
+     await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+}));
+
+module.exports = router;
