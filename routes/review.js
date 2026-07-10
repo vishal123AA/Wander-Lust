d42e724eb4f7e6e675b5e2924ce50ef3268ff25d
vishal <vishalkumarkalher16@gmail.com>
2026-07-10 22:33:33 +0530

    in this we use req.flash for the  create  and  delete reviews

--- routes/review.js
+++ routes/review.js
@@ -30,6 +30,7 @@ router.post("/",validateReview,wrapAsync( async(req,res) => {
     await newReview.save();
     await listing.save();
 
+    req.flash("success"," New Review Created!");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -39,6 +40,7 @@ router.delete("/:reviewId" ,wrapAsync(async(req,res) => {
     await Listing.findByIdAndUpdate(id , {$pull:{reviews :reviewId}});
     await Review.findByIdAndDelete(reviewId);
 
+    req.flash("success","Review Deleted!");
     res.redirect(`/listings/${id}`);
 }));
 
