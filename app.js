0cc6239aabe45ba436baa0f6d6b605cb88d2ba58
vishal <vishalkumarkalher16@gmail.com>
2026-07-16 13:01:47 +0530

    small fixes and uses the express routers for the user

--- app.js
+++ app.js
@@ -83,14 +83,15 @@ app.use((req,res,next) =>{
     next();
 });
 
-app.get("/demoUser", async(req,res) => {
-    let fakeUser = new User({
-        username : "vishal",
-        email : "vish@gmail.com"
-    });
-    let registerdUser =   await User.register(fakeUser,"hello");
-    res.send(registerdUser);
-});
+// app.get("/demoUser", async(req,res) => {
+//     let fakeUser = new User({
+//         username : "vishal",
+//         email : "vish@gmail.com"
+//     });
+//     let registerdUser =   await User.register(fakeUser,"hello");
+//     res.send(registerdUser);
+// });
+
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
