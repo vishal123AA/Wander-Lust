5b8a957d645ef0a9c27d3a7f5ae757eceee2f348
vishal <vishalkumarkalher16@gmail.com>
2026-07-16 13:03:23 +0530

    in this we add the route for /signup post request and uses joi validation for uservalidation and add user to DB

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,47 @@
 const express = require("express");
 const router = express.Router();
 
-// const User = require("../models/user");
+const User = require("../models/user");
+const wrapAsync = require("../utils/wrapAsync");
+const ExpressError = require("../utils/ExpressError.js");
 
+const {userSchema} = require("../schema.js"); 
+
+const validateUser = (req,res,next)=>{
+    let {error} =  userSchema.validate(req.body);
+      if(error){
+        let errMsg = error.details.map((el) =>  el.message).join(",");
+        throw new ExpressError(400,errMsg);
+      }
+      else{
+        next();
+      }
+}
+
+//signUp user
 router.get("/signUp", (req,res) => {
     res.render("users/signUp.ejs");
+});
+
+//add user
+router.post("/signUp",validateUser,wrapAsync(async(req,res) =>{
+    try{ 
+        const{username,password,email} = req.body.user;
+        const newUser = new User({email,username});
+        const registerdUser = await User.register(newUser,password);
+        console.log(registerdUser);
+        req.flash("success","Welcome to Wanderlust");
+        res.redirect("/listings");
+    }
+    catch(e){
+        req.flash("error", e.message);
+        res.redirect("/signup") 
+    }
+}));
+
+//login user
+router.get("/login", (req,res) => {
+    res.render("users/login.ejs");
 });
 
 module.exports = router;
