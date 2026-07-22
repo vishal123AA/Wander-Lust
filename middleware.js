f7721fc8f76ef8479bcd03163e1e64c96f932a27
vishal <vishalkumarkalher16@gmail.com>
2026-07-22 10:39:51 +0530

    in this we create an middleware isowner which checks that if the listing owner is ame with the logged in user

--- middleware.js
+++ middleware.js
@@ -1,3 +1,5 @@
+const Listing = require("./models/listing");
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.redirectUrl = req.originalUrl;
@@ -10,6 +12,16 @@ module.exports.isLoggedIn = (req, res, next) => {
 module.exports.saveRedirectUrl = (req,res,next) =>{
     if(req.session.redirectUrl){
         res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+};
+
+module.exports.isOwner = async(req,res,next) => {
+    let {id} = req.params;
+    let listing = await Listing.findById(id);
+    if (!listing.owner._id.equals(res.locals.currUser._id)) {
+        req.flash("error","you are not owner of this listing");
+        return res.redirect(`/listings/${id}`);
     }
     next();
 };
