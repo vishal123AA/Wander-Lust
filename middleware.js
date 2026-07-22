76cea5ec2c10ee6fb71f32f191e1fc43eb2fd0c6
vishal <vishalkumarkalher16@gmail.com>
2026-07-22 21:31:41 +0530

    in this we add the validate review middleware to validate the reviews

--- middleware.js
+++ middleware.js
@@ -37,4 +37,15 @@ module.exports.validateListing = (req,res,next)=>{
       else{
         next();
       }
-}
+};
+
+module.exports.validateReview = (req,res,next)=>{
+    let {error} =  reviewSchema.validate(req.body);
+      if(error){
+        let errMsg = error.details.map((el) =>  el.message).join(",");
+        throw new ExpressError(400,errMsg);
+      }
+      else{
+        next();
+      }
+};
