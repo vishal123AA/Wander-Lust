018bd07e9528e967b62a7c2334cf3e3ab163e8ec
vishal <vishalkumarkalher16@gmail.com>
2026-07-22 21:20:26 +0530

    in this we add the validateListing middleware which is used to validate the listing

--- middleware.js
+++ middleware.js
@@ -1,4 +1,6 @@
 const Listing = require("./models/listing");
+const {listingSchema, reviewSchema} = require("./schema.js");
+const ExpressError = require("./utils/ExpressError.js");
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
@@ -25,3 +27,14 @@ module.exports.isOwner = async(req,res,next) => {
     }
     next();
 };
+
+module.exports.validateListing = (req,res,next)=>{
+    let {error} =  listingSchema.validate(req.body);
+      if(error){
+        let errMsg = error.details.map((el) =>  el.message).join(",");
+        throw new ExpressError(400,errMsg);
+      }
+      else{
+        next();
+      }
+}
