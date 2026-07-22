3ddb701fdb034eb6c0b82c608e5382051443bcdb
vishal <vishalkumarkalher16@gmail.com>
2026-07-22 21:32:31 +0530

    in this we use validateReview as an middleware to validate the review

--- routes/review.js
+++ routes/review.js
@@ -2,23 +2,13 @@ const express = require("express");
 const router = express.Router({mergeParams:true});
 
 const Review = require("../models/review.js");
-const {reviewSchema} = require("../schema.js");
-
 const Listing = require('../models/listing');
 
-const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
+const {validateReview} = require("../schema.js");
 
-const validateReview = (req,res,next)=>{
-    let {error} =  reviewSchema.validate(req.body);
-      if(error){
-        let errMsg = error.details.map((el) =>  el.message).join(",");
-        throw new ExpressError(400,errMsg);
-      }
-      else{
-        next();
-      }
-}
+const wrapAsync = require("../utils/wrapAsync.js");
+
+
 
 //post review add
 router.post("/",validateReview,wrapAsync( async(req,res) => {
