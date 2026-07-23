c796aefec69574e7459e14cd22ee4530e7327db0
vishal <vishalkumarkalher16@gmail.com>
2026-07-23 16:06:41 +0530

    in this we require our callbacks/functions from the controller review.js and use them to make our code modular and design MVC

--- routes/review.js
+++ routes/review.js
@@ -1,38 +1,16 @@
 const express = require("express");
 const router = express.Router({mergeParams:true});
 
-const Review = require("../models/review.js");
-const Listing = require('../models/listing');
-
 const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
+
+const reviewController = require("../controllers/review.js");
 
 const wrapAsync = require("../utils/wrapAsync.js");
 
-
-
 //post review add
-router.post("/",isLoggedIn,validateReview,wrapAsync( async(req,res) => {
-    let {id} = req.params;
-    let listing =  await Listing.findById(id);
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;
-
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-
-    req.flash("success"," New Review Created!");
-    res.redirect(`/listings/${id}`);
-}));
+router.post("/",isLoggedIn,validateReview,wrapAsync( reviewController.createReview));
 
 //delete the review
-router.delete("/:reviewId",isLoggedIn,isReviewAuthor ,wrapAsync(async(req,res) => {
-    let {id,reviewId} = req.params;
-    await Listing.findByIdAndUpdate(id , {$pull:{reviews :reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-
-    req.flash("success","Review Deleted!");
-    res.redirect(`/listings/${id}`);
-}));
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor ,wrapAsync(reviewController.destroyReview));
 
 module.exports = router;
