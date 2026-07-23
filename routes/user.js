7f8369b8163e4fa56be4ccc8c5d8438e9e6251aa
vishal <vishalkumarkalher16@gmail.com>
2026-07-23 19:22:16 +0530

    resolve the bug

--- routes/user.js
+++ routes/user.js
@@ -11,13 +11,13 @@ const userController = require("../controllers/user.js")
 
 //here we combine the route signUp user and add user
 router.route("/signUp")
-.get("/signUp", userController.renderSignupForm )
-.post("/signUp",validateUser,wrapAsync(userController.signup));
+.get( userController.renderSignupForm)
+.post(validateUser,wrapAsync(userController.signup));
 
 //here we combine the login form and login user
 router.route("/login")
-.get("/login", userController.renderLoginform)
-.post("/login",saveRedirectUrl,passport.authenticate('local',{failureFlash:true , failureRedirect : "/login"}),
+.get( userController.renderLoginform)
+.post(saveRedirectUrl,passport.authenticate('local',{failureFlash:true , failureRedirect : "/login"}),
 userController.login);
 
 //logout user
