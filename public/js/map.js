a21346aa12d902931d0290d4ac1944c601939a3a
vishal <vishalkumarkalher16@gmail.com>
2026-08-06 23:58:50 +0530

    in this we add an marker popup on every marker which pop up the listing title and a message

--- public/js/map.js
+++ public/js/map.js
@@ -2,10 +2,14 @@
  const map = new mapboxgl.Map({
     container: 'map', // container ID
     style : "mapbox://styles/mapbox/streets-v12",//style url
-    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9 // starting zoom
  });
 
  const marker1 = new mapboxgl.Marker({color: "red"})
-        .setLngLat(coordinates)
+        .setLngLat(listing.geometry.coordinates)
+        .setPopup(
+             new mapboxgl.Popup({offset: 25})
+             .setHTML(`<h4>${listing.title}</h4><p>Exact Location will be provide after booking`)
+        )
         .addTo(map);
