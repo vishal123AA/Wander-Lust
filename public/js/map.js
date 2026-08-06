1a669adc418ea5afb40e7822241bc0249ffc6525
vishal <vishalkumarkalher16@gmail.com>
2026-08-07 00:04:53 +0530

    changed the map style

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,7 @@
  mapboxgl.accessToken = mapToken;
  const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style : "mapbox://styles/mapbox/streets-v12",//style url
+    style : "mapbox://styles/mapbox/outdoors-v12",//style url
     center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9 // starting zoom
  });
