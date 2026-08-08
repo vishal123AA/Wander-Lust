2991939c94b8cd1254291ed61588cb1c6f473069
vishal <vishalkumarkalher16@gmail.com>
2026-08-08 10:48:21 +0530

    resolve a small bug of maptoken credential

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,7 +1,7 @@
 const Listing = require("../models/listing");
 
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const mapToken  = process.env.Map_TOKEN;
+const mapToken  = process.env.MAP_TOKEN;
 const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 const { findById } = require("../models/review");
